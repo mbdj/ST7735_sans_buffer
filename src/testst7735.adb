f875f17f9fbfd0fba54558534c7a6b2362313222
mehdi <mbdj@free.fr>
2023-02-05 18:12:57 +0100

    orientation

--- src/testst7735.adb
+++ src/testst7735.adb
@@ -30,6 +30,12 @@ with Ravenscar_Time;
 
 procedure Testst7735 is
 
+	Hauteur : constant Natural := 160;
+
+	--  sur l'écran 80x160 il y a un offset à l'affichage ; pas résolu ...
+	Offset_X : constant Natural := 26;
+	Offset_Y : constant Natural := 2;
+
 	--  dimensions de l'écran ST7735
 	Period       : constant Ada.Real_Time.Time_Span := Ada.Real_Time.Milliseconds (50);
 	Next_Release : Ada.Real_Time.Time := Ada.Real_Time.Clock;
@@ -43,8 +49,9 @@ procedure Testst7735 is
 										 SPI_SCK          => STM32.Device.PB13'Access,  -- à raccorder à SCK ou SCL       (SPI1 : PA5 ; SPI2 : PB13)
 										 SPI_MISO         => STM32.Device.PB14'Access,  -- pas utilisé sur l'écran ST7735 (SPI1 : PA7 ; SPI2 : PB14)
 										 SPI_MOSI         => STM32.Device.PB15'Access,  -- à raccorder à SDA              (SPI1 : PA7 ; SPI2 : PB15)
-										 Width            => 128,              -- backlight (LEDA ou BLK) doit être raccordé à +3.3V ou +5V
+										 Width            => Hauteur + Offset_Y,              -- backlight (LEDA ou BLK) doit être raccordé à +3.3V ou +5V
 										 Height           => 160,
+										 Orientation      => portrait,
 										 Color_Correction => True);
 
 	Compteur : Natural := 0; --  compteur affiché sur le ST7735
@@ -67,7 +74,7 @@ begin
 	Ecran_ST7735.BitMap.Set_Source (ARGB => HAL.Bitmap.Purple);
 	Ecran_ST7735.BitMap.Fill;
 	Bitmapped_Drawing.Draw_String (Ecran_ST7735.BitMap.all,
-										  Start      => (0, 0),
+										  Start      => (Offset_X, Offset_Y),
 										  Msg        => ("ST7735"),
 										  Font       => BMP_Fonts.Font8x8,
 										  Foreground => HAL.Bitmap.Red,
@@ -77,13 +84,13 @@ begin
 		STM32.Board.Toggle (STM32.Board.Green_LED);
 
 		Bitmapped_Drawing.Draw_String (Ecran_ST7735.BitMap.all,
-											Start      => (0, PosY),
+											Start      => (Offset_X, PosY + Offset_Y + 8),
 											Msg        => (Compteur'Image),
 											Font       => BMP_Fonts.Font12x12,
 											Foreground => HAL.Bitmap.Red,
 											Background => HAL.Bitmap.Green);
 
-		PosY := (if PosY > 160 then 0 else PosY + 1);
+		PosY := (if PosY > Hauteur then 0 else PosY + 1);
 
 		Compteur := Compteur + 1 ;
 
