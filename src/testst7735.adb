842b70b913aacd261be9786ffb2806d676980b4d
mehdi <mbdj@free.fr>
2023-02-25 20:06:32 +0100

    final

--- src/testst7735.adb
+++ src/testst7735.adb
@@ -35,7 +35,7 @@ procedure Testst7735 is
 	function Max (A, B : in Natural) return Natural is (if A > B then A else B);
 	----------------------------------------------------
 
-	Width       :  constant Natural := 80;
+	Width       :  constant Natural := 128;
 	Height      :  constant Natural := 160;
 	Orientation : constant Type_Orientation := Landscape;
 
